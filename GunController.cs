88f1199d37a9bd703e80408d760287f5ff136af3
hkt1427 <118532826+hkt1427@users.noreply.github.com>
2022-11-21 03:11:22 +0900

    Create GunController.cs

---
+++ GunController.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunController : MonoBehaviour
+{
+    public Transform weaponHold;
+    public Gun[] allweapons;
+    Gun equippedGun;
+
+    public void EquipGun(Gun GunToEquip)
+    {
+        if (equippedGun != null)
+        {
+            Destroy(equippedGun.gameObject);
+        }
+        equippedGun = Instantiate(GunToEquip, weaponHold.position, weaponHold.rotation) as Gun;
+        equippedGun.transform.parent = weaponHold;
+    }
+
+    public void EquipGun(int weaponIndex)
+    {
+        EquipGun(allweapons [weaponIndex]);
+    }
+
+    public void OnTriggerHold()
+    {
+        if (equippedGun != null)
+        {
+            equippedGun.OnTriggerHold();
+        }
+    }
+    public void OnTriggerRelease()
+    {
+        if (equippedGun != null)
+        {
+            equippedGun.OnTriggerRelease();
+        }
+    }
+
+    public float GunHeight
+    {
+        get
+        {
+            return weaponHold.position.y;
+        }
+    }
+
+    public void Aim(Vector3 aimPoint)
+    {
+        if (equippedGun != null)
+        {
+            equippedGun.Aim(aimPoint);
+        }
+    }
+
+    public void Reload()
+    {
+        if (equippedGun != null)
+        {
+            equippedGun.Reload();
+        }
+    }
+}
